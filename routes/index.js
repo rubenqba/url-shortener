c836f5feb9476a0e8cdca924a3a178c7d8b42331
Ruben Bresler <14687-rubenqba@users.noreply.gitlab.com>
2021-10-13 13:23:42 -0500

    added CORS support and using `nanoid` package for ID generation

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const validUrl = require('valid-url');
-const shortid = require('shortid');
 const config = require('config');
 
 const Url = require('../model/url');
